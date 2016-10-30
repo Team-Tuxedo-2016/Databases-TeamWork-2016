85dd840405d75e82cdebc3b332d55e4fe3c93b25
iliangogov <iliangogov@gmail.com>
2016-10-30 15:33:58 +0200

    EventHandlers added to the buttons, Console output notifications added

--- StartUpWPF/MainWindow.xaml.cs
+++ StartUpWPF/MainWindow.xaml.cs
@@ -29,5 +29,35 @@ namespace StartUpWPF
         {
             
         }
+
+        public void OnGetExcellFromZip(object sender, RoutedEventArgs e)
+        {
+            //TODO: implement in separate project (for example .Logic.dll) and call proper class instance or method 
+            Console.WriteLine("Excell data was transfered from Zip to SQL db");
+        }
+
+        public void OnGetJsonReportFromDb(object sender, RoutedEventArgs e)
+        {
+            //TODO: implement and call proper class instance or method
+            Console.WriteLine("JSON report file was created from SQL db");
+        }
+
+        public void OnGetXmlReportFromDb(object sender, RoutedEventArgs e)
+        {
+            //TODO: implement and call proper class instance or method
+            Console.WriteLine("XML report file was created from SQL db");
+        }
+
+        public void OnGetPdfReportFromDb(object sender, RoutedEventArgs e)
+        {
+            //TODO: implement and call proper class instance or method
+            Console.WriteLine("Pdf report file was created from SQL db");
+        }
+
+        public void OnGetExcellReportFromDb(object sender, RoutedEventArgs e)
+        {
+            //TODO: implement and call proper class instance or method
+            Console.WriteLine("EXCELL report file was created from SQL db");
+        }
     }
 }
